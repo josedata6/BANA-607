ebb33140ecec501ac4a8dd312c996f6c89ead1bc
josedata6 <jose.diaz@csun.edu>
2024-09-05 19:50:39 -0700

    professors example

--- exe_1.py
+++ exe_1.py
@@ -7,3 +7,12 @@ result = num1 * num2
 
 # Print the result
 print("The result of multiplying", num1, "and", num2, "is", result)
+
+
+x=int(input("Enter your first number: "))
+print(x)
+y=22
+z=int(x+y)
+print(z)
+
+
