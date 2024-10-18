3dc25b737c61a6390985eb91808e46c860388c39
josedata6 <jose.diaz@csun.edu>
2024-10-18 09:42:56 -0700

    updates

---
+++ week-8/2017golf-stats.py
@@ -0,0 +1,32 @@
+import numpy as np
+import pandas as pd
+
+
+# # Load data
+housing_df = pd.read_csv('2016-golf-stats.csv') # importing the csv file
+
+# #Renaming Columns
+housing_df.columns = [s.strip().replace(' ', '_') for s in housing_df.columns] # all columns
+
+# # Descriptive statistics
+print(housing_df.describe()) # show summary statistics for each column
+
+
+# df = pd.DataFrame(housing_df)
+# print(df)
+
+# #Finding Correlation
+# correlation_matrix = df.corr()
+# print(correlation_matrix)
+
+## saving output into file
+# correlation_matrix.to_csv('correlationMatrix.csv')
+
+# # Visualize the finding
+# import seaborn as sn
+# import matplotlib.pyplot as plt
+
+
+# sn.heatmap(correlation_matrix, annot=True)
+# plt.show()
+
---
+++ week-8/week8_exter.py
@@ -0,0 +1,6 @@
+def sumFunc(a,b):
+    c=a*b
+    return c
+
+d=sumFunc(5,2)
+print(d)
